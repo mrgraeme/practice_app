2e7f5314cf29f885e26b1aefb0ee4e89bf2f2d5d
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2012-06-02 22:22:45 +0100

    added signin page with functionality limited to rendering new page - part way through 8.1.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 PracticeApp::Application.routes.draw do
   
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  match '/signup', to: 'users#new'
+
 
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
-  
-  match '/signup', to: 'users#new'
-  
+
+
   
 
   # The priority is based upon order of creation:
